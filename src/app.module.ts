6bed53cd1b245279d921dbb0226152be8c49a4fe
silasoli <silasoliv39@gmail.com>
2024-10-10 15:47:40 -0300

    fix: error

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,5 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { SolutionsModule } from './solutions/solutions.module';
 import { DemonstrationsModule } from './demonstrations/demonstrations.module';
 import { CausesModule } from './causes/causes.module';
