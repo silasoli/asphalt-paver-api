2a5e299abb4dfa7eada2021dbf95a7e2a9e103e7
silasoli <silasoliv39@gmail.com>
2024-11-21 17:52:05 -0300

    remove default routes

--- src/cloud-flare/controllers/cloud-flare.controller.ts
+++ src/cloud-flare/controllers/cloud-flare.controller.ts
@@ -26,79 +26,12 @@ import { UploadImgCloudFlareDto } from '../dto/update-img-cloud-flare.dto';
 import { CLOUD_FLARE_ERRORS } from '../constants/cloud-flare.errors';
 import { UploadImgCloudFlareResponseDto } from '../dto/update-img-cloud-flare-response.dto';
 import { IDQueryDTO } from '../../common/dto/id-query.dto';
-import { FileInterceptor2 } from '../file.interceptor';
-
-export interface FileInfo {
-  fieldName: string;
-  originalName: string;
-  encoding: string;
-  mimeType: string;
-  size: number;
-  buffer: Buffer;
-}
-
-export class UploadImgBase64Dto {
-  @ApiProperty({ description: 'Base64 encoded image string' })
-  // @IsNotEmpty()
-  // @IsBase64()
-  base64Image: string;
-
-  @ApiProperty({ description: 'Image file name' })
-  // @IsNotEmpty()
-  // @IsString()
-  fileName: string;
-
-  @ApiProperty({ description: 'Image MIME type (e.g., image/png, image/jpeg)' })
-  // @IsNotEmpty()
-  // @IsString()
-  mimeType: string;
-}
 
 @ApiBearerAuth()
 @ApiTags('Images')
 @Controller('image')
 export class CloudFlareController {
   constructor(private readonly service: CloudFlareService) {}
-
-  @Post('upload-base64')
-  @ApiConsumes('application/json')
-  @ApiOperation({ summary: 'Upload an image encoded in Base64' })
-  @ApiBody({ type: UploadImgBase64Dto })
-  async uploadBase64(
-    @Body() uploadImgBase64Dto: UploadImgBase64Dto,
-  ): Promise<UploadImgCloudFlareResponseDto> {
-    const { base64Image, fileName, mimeType } = uploadImgBase64Dto;
-
-    // Convert Base64 to Buffer
-    const fileBuffer = Buffer.from(base64Image, 'base64');
-
-    return this.service.uploadImage2({
-      originalname: fileName,
-      mimetype: mimeType,
-      buffer: fileBuffer,
-      size: fileBuffer.length,
-    });
-  }
-
-  @Post('upload/alternative')
-  @UseInterceptors(FileInterceptor2())
-  async upload(
-    @Body() body: any,
-    @UploadedFile(
-      new ParseFilePipe({
-        validators: [
-          new MaxFileSizeValidator({
-            maxSize: 1000000,
-            message: 'File should be less than 1MB please',
-          }),
-          new FileTypeValidator({ fileType: 'image/jpeg' }),
-        ],
-      }),
-    )
-    file: FileInfo,
-  ) {
-    console.log({ ...body, filename: file.originalName });
-  }
 
   @ApiOperation({ summary: 'Realizar Upload de imagem' })
   @ApiResponse({
